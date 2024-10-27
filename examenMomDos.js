2bb67f8ae3809032980a30096c846d926e4225ad
MiguelHerreraGK13 <miguelherrera.gk1@gmail.com>
2024-10-27 13:12:57 -0500

    se valido y corrige el ingreso a validacion de valores de los productos.

--- examenMomDos.js
+++ examenMomDos.js
@@ -1,7 +1,7 @@
 //variables para la ejecucion
 let username
 let password
-let intentos = 1
+let intentos = 0
 const maxIntentos = 3
 let registros
 let inventario=100
@@ -15,15 +15,16 @@ do {
         if (username === "admin" && password === "1234") {
             registros = 1
             while (registros == 1) {
-            let producto
-                producto = parseInt(prompt("ingrese el valor del producto $"));
-                if (producto !== Number) {
+            let producto=0
+                producto = (prompt("Enter the product value $"));
+                producto=parseFloat(producto)
+                if (isNaN(producto)) {
+                    console.log("the value is not a number, try again");
+                }else {
                 
 
 
                 inventario--
-                }else {
-                console.log("incorrect data, try again")
                 }
             registros= parseInt(prompt("do you want to registrate a new product?\n press 1 to registrate\n press 2 for exit"))
         }
