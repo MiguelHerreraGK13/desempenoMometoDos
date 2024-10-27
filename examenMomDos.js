2bf3457b7f3c2f8b517112b4ef6d39552c72751c
MiguelHerreraGK13 <miguelherrera.gk1@gmail.com>
2024-10-27 12:36:44 -0500

    comienzo desarrollo del la logica interna, validacion de ingreso de valores numericos variable producto

--- examenMomDos.js
+++ examenMomDos.js
@@ -1,37 +1,36 @@
 //variables para la ejecucion
 let username
 let password
-let intentos=1
-cont maxIntentos=3
+let intentos = 1
+const maxIntentos = 3
+let registros
+let inventario=100
 
 //autenticacion
 
 do {
-    
-    
-    username= prompt('please get in your userName')
-    password= prompt("please get in your password")
+        username = prompt('please get in your userName')
+        password = prompt("please get in your password")
 
-    if (username=== "admin"&& password==="1234") {
-
-        
-
-
-    
-
-    }
-    break
-} while (intentos!==maxIntentos);
+        if (username === "admin" && password === "1234") {
+            registros = 1
+            while (registros == 1) {
+            let producto
+                producto = parseInt(prompt("ingrese el valor del producto $"));
+                if (producto !== Number) {
+                
 
 
+                inventario--
+                }else {
+                console.log("incorrect data, try again")
+                }
+            registros= parseInt(prompt("do you want to registrate a new product?\n press 1 to registrate\n press 2 for exit"))
+        }
+    } else {
+    intentos++}
 
-else {
-    intentos++
+}while (intentos!==maxIntentos);
 
-    
-    
-    
-    
-}
 
-console.log("Restart the sistem again");
+console.log("Restart the sistem again")
